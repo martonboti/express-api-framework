9988d1b4e7bd944269cf71bccc82e1e4fd4d8325
unknown <martonbotix@gmail.com>
2018-05-01 10:37:01 +0300

    Formatted 404 response for non-existing pages

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const port = process.env.PORT;
 const index = require('./routes/index');
 
 app.use('/', index);
+app.use((req, res) => {
+  res.status(404).send({
+    error: {
+      message: '404 Not Found',
+      status_code: 404
+    }
+  });
+});
 
 app.listen(port);
 
